533560de49a22488f80844f79e318326087353fb
Nozomi Sugiyama <rioc.sugiyama@gmail.com>
2018-07-04 15:01:06 +0900

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ const getEmailMessage = (userName, confirmationCode) => (`
 exports.handler = (event, context, callback) => {
     if(event.userPoolId === userPoolId) {
         if(event.triggerSource === "CustomMessage_SignUp") {
+            console.log(event.request);
             event.response.emailSubject = "Welcome to Portal! Please verify your Email Adress.";
-            event.response.emailMessage = getEmailMessage(event.request.usernameParameter, event.request.codeParameter);
+            event.response.emailMessage = getEmailMessage(event.userName, event.request.codeParameter);
         }
     }
     
